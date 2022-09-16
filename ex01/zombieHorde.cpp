80fc8e61f1f470a26e8f0af79050ff7de4c236cf
Hugo Cremers <hcremers@student.s19.be>
2022-09-16 17:13:30 +0200

    cpp01 v1.1: - added comment in ex01 after evaluation (forbidden function in c++98)

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: hcremers <hcremers@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 15:37:47 by hcremers          #+#    #+#             */
-/*   Updated: 2022/09/13 16:47:14 by hcremers         ###   ########.fr       */
+/*   Updated: 2022/09/15 16:06:00 by hcremers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ Zombie*	zombieHorde(int N, std::string name)
 	
 	for(int i = 0; i < N ; i++)
 	{
-		zom[i].setName(name + std::to_string(i));
+		zom[i].setName(name + std::to_string(i)); // to_string: forbidden function in c++98
 		zom[i].announce();
 	}
 	return (zom);
